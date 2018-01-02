c19c03f6a8bda065a76d23587df127c53d095de2
jaguar1014 <alyssahester@Alyssas-MBP.fios-router.home>
2018-01-01 21:00:53 -0500

    updating information"

--- js/customfunctions.js
+++ js/customfunctions.js
@@ -1,35 +1,6 @@
 $(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+	handleNavbar();
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 200, function(){
-   
-       
-      });
-    } 
-  });
-  	function sectionHeaderChange (section, quote) {
-  		var sectionHeader = $("#" + section + " > h2").text();
-		$("#" + section + " > h2").hover(function() {
-			$(this).css("font-family","Exo,sans-serif");
-			$(this).text(quote);
-		}, function() {
-			$(this).css("font-family","Lobster,cursive");
-			$(this).text(sectionHeader);
-		});
-	};
 
 	sectionHeaderChange("skills","The world is before you and you need not take it or leave it as it was when you came in. - James Baldwin");
 	sectionHeaderChange("education","It was books that taught me that the things that tormented me most were the very things that connected me with all the people who were alive, or who had ever been alive. - James Baldwin");
@@ -56,3 +27,49 @@ $(document).ready(function(){
 
 
 });
+
+function handleNavbar() {
+	/* Adds smooth scrolling when link is clicked
+
+	*/
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 200, function(){
+   
+       
+      });
+    } 
+  });
+
+  /*Close mobile nav menu on click*/
+
+    $(document).on('click','.navbar-collapse.in',function(e) {
+	    if( $(e.target).is('a') ) {
+	        $(this).collapse('hide');
+	    }
+	});
+}
+
+
+function sectionHeaderChange (section, quote) {
+  	var sectionHeader = $("#" + section + " > h2").text();
+	$("#" + section + " > h2").hover(function() {
+		$(this).css("font-family","Exo,sans-serif");
+		$(this).text(quote);
+	}, function() {
+		$(this).css("font-family","Lobster,cursive");
+		$(this).text(sectionHeader);
+	});
+};
